94a00cb14b02936617f2e7d57a8d082f68234395
Jan-Pieter Baert <jan-pieter@baert.jp.net>
2021-07-19 21:49:10 +0200

    Add more delta notify rules

--- config/delta/rules.js
+++ config/delta/rules.js
@@ -1,6 +1,22 @@
 export default [{
     match: {
         // form of element is {subject,predicate,object}
+        object: {
+            value: "http://mu.semte.ch/vocabularies/cache/CacheClear"
+        }
+    },
+    callback: {
+        url: "http://cache_clear/.mu/delta",
+        method: "POST"
+    },
+    options: {
+        resourceFormat: "v0.0.1",
+        gracePeriod: 1000,
+        ignoreFromSelf: true
+    }
+}, {
+    match: {
+        // form of element is {subject,predicate,object}
         predicate: {
             value: "http://mu.semte.ch/vocabularies/push/tabId",
             type: "uri"
@@ -13,6 +29,19 @@ export default [{
     options: {
         resourceFormat: "v0.0.1",
         gracePeriod: 1000,
+        ignoreFromSelf: true
+    }
+}, {
+    match: {
+        subject: {}
+    },
+    callback: {
+        url: "http://resource/.mu/delta",
+        method: "POST"
+    },
+    options: {
+        resourceFormat: "v0.0.1",
+        gracePeriod: 250,
         ignoreFromSelf: true
     }
 }]
